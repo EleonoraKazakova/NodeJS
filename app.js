447e35539d6673462524a9ed77b67b2325adc48f
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-02-09 21:04:30 +0100

    Add routes

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express')
 const parsedBody = require('body-parser')
+
 const app = express()
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(parsedBody.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Product page!')
-    
-    res.send('<body><form action="/product" method="POST"> <input type="text" name="title"><button type="submit">Send</button></form><body>')
-
-} )
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    console.log('In another middleware!')
-    res.send('<h1>Hello from Express!</h1>')
-} )
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
 
---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const express = require('express')
+const { dirname } = require('path')
+const router = express.Router()
+  
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    console.log('Product page!')
+    
+    // res.send('<body><form action="/admin/add-product" method="POST"> <input type="text" name="title"><button type="submit">Send</button></form><body>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+} )
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})  
+
+module.exports = router
