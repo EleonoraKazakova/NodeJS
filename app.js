1d9dc1f6c1131b0906ab21032cbfd9cbc41587d5
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-03-16 20:13:45 +0100

    Add novigation

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const path = require('path')
+const path = require("path");
 
-const express = require('express')
-const parsedBody = require('body-parser')
+const express = require("express");
+const parsedBody = require("body-parser");
 
+const app = express();
 
-const app = express()
+app.set("view engine", "ejs"); // compile tamplate
+app.set("views", "views"); // where to finedƒ
 
-app.set('view engine', 'ejs') // compile tamplate
-app.set('views', 'views') // where to fined
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.use(parsedBody.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(parsedBody.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'})
-   // res.status(404).send('<h1>Page not found</h1>')
-   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+  res.status(404).render("404", { pageTitle: "Page not found", path: 'Error' });
 
-app.listen(3000)
+});
 
+app.listen(3000);
 
 /*
 handlebar
@@ -39,7 +36,6 @@ app.engine(
     console.log('This always runs!')
     next() // Alows the request to continue to the next middleware in line
 } ) */
-
 
 // const http = require('http')
 // const server = http.createServer(app)
