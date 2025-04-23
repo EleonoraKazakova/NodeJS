36d37502a9f90865c53b845519f4aa002c11c531
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-04-23 15:21:14 +0200

    Storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,8 +10,14 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title
+    const imageUrl = req.body.imageUrl
+    const price = req.body.price
+    const description = req.body.description
+
     console.log('postAddProduct req.body: ', req.body)
-    const product = new Product(req.body.title)
+    
+    const product = new Product(title, imageUrl, price, description)
     product.save()
     res.redirect('/')
 }
