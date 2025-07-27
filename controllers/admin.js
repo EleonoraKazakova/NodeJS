7e5cc6d5b779825bdd6ca9a6de7667a2d99231a9
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-07-27 22:12:48 +0200

    Add edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,11 +3,10 @@ const Product = require('../models/product')
 exports.getAddProduct = (req, res, next) => {
     console.log('getAddProduct')
     
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: "Add products", 
         path:'/admin/add-product', 
-        productCSS: true, 
-        activeAddProduct: true
+        editing: false,
     })
 }
 
@@ -23,6 +22,27 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, price, description)
     product.save()
     res.redirect('/')
+}
+
+exports.getEditProduct = (req, res, next) => {
+    console.log('getEditProduct')
+    const editMode = req.require.edit
+    if(!editMode) {
+        return res.redirect('/')
+    }
+    const prodId = req.param.productId
+    Product.findByID(prodId, product => {
+        if(!product) {
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: "Edit products", 
+            path:'/admin/edit-product', 
+            editing: editMode,
+            product: product,
+        })
+    })
+    
 }
 
 exports.getProducts = (req, res, next) => {
