7cbb380f493f9ba125ea09f00c73d223b507e9ce
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-08-31 20:22:16 +0200

    Add Sequelize

--- util/database.js
+++ util/database.js
@@ -1,4 +1,11 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-complete', 'root', 'node-complete', {
+    dialect: 'mysql', host: 'localhost'
+})
+
+module.exports = sequelize
+
+/*const mysql = require('mysql2')
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -7,4 +14,4 @@ const pool = mysql.createPool({
     password: 'node-complete'
 })
 
-module.exports = pool.promise()
+module.exports = pool.promise() */
