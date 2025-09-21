2fe276927d576f26700194b90b4a301d4e30a209
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2025-09-21 17:04:50 +0200

    Delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -26,6 +26,7 @@ exports.postAddProduct = (req, res, next) => {
     })
         .then(result => {
            console.log('Created Product')
+           res.redirect('/admin/products')
         })
         .catch(err => console.error(err))
     
@@ -75,8 +76,6 @@ exports.postEditProduct = (req, res, next) => {
             res.redirect('/admin/products') 
         })
         .catch(err => console.error(err))
-    
-   
 }
 
 exports.getProducts = (req, res, next) => {
@@ -95,6 +94,12 @@ exports.getProducts = (req, res, next) => {
 
  exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId
-    Product.deleteById(prodId)
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+        .then(product => {
+            return product.destroy()
+        })
+        .then(result => {
+            console.log('Destroyed Product!')
+            res.redirect('/admin/products')
+        }).catch(err => console.error(err))
  }
