31ef4b40cbef9abfef444ee42940d18e008b65c7
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-03-15 17:43:58 +0100

    Creating the Product Schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema 
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+})
 /*const mongodb = require('mongodb')
 const { DOT_NEXT_ALIAS } = require('next/dist/lib/constants')
 const getDb = require('../util/database').getDb
