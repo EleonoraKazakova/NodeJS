a28213372a9c7f4197a8da2bd4c3f9bcc582fa86
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-03-15 15:09:22 +0100

    Getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -118,7 +118,7 @@ exports.getProducts =  (req, res, next) => {
 
   exports.getOrders = (req, res, next) => {
     req.user
-      .getOrders({include: ['products']})
+      .getOrders()
       .then(orders => {
         res.render('shop/orders', {
           path: '/orders',
