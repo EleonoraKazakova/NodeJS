e8aaf226834ee0a3537072cbe6d2851e461d493c
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-05-03 20:26:07 +0200

    Adding logic to Update the Password

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         required: true
     },
     resetToken: String,
-    reserTokenExpiration: Date,
+    resetTokenExpiration: Date,
     cart: {
         items: [
             { 
