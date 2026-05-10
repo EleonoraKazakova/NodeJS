2bcda07de38c74886f8b81a18c493a79d7465518
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-05-10 17:17:33 +0200

    Using Validation error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -97,7 +97,7 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/login', {
             path: '/login',
             pageTitle: 'Login',
-            errorMessage: errors.array()
+            errorMessage: errors.array()[0].msg
         });
     }
 
